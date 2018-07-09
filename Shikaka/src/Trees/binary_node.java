f84d716d32bc5eef98ff72dd7d45bc0254221ec5
mauryasameer <sameer.apsdk@gmail.com>
2018-07-10 01:07:32 +0530

    node

--- Shikaka/src/Trees/binary_node.java
+++ Shikaka/src/Trees/binary_node.java
@@ -1,5 +1,11 @@
 package Trees;
 
-public class binary_node {
-
+public class binary_node<T> {
+	int data;
+	binary_node left;
+	binary_node right;
+	binary_node(T data){
+		data=this.data;
+	}
+	
 }
