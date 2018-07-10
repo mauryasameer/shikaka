6e77a1aa497a17975f5ebc2ad1710f9281b4c9f5
mauryasameer <sameer.apsdk@gmail.com>
2018-07-10 14:57:12 +0530

    more tree stuff

--- Shikaka/src/Trees/binary_node.java
+++ Shikaka/src/Trees/binary_node.java
@@ -1,12 +1,37 @@
 package Trees;
 
-public class binary_node<T> {
-	T data;
+public class binary_node {
+	int data;
 	binary_node left;
 	binary_node right;
-	binary_node(T data){
-		data=this.data;
-		
+	binary_node(int i){
+		this.data=i;
+		right = null;
+		left = null;
 	}
-	//this will create a node of binary tree
+	public binary_node() {
+		left = null;
+		right= null;
+		data = 0;
+	}
+	public void set_Left(binary_node n){
+		left = n;
+	}
+	public void set_right(binary_node n){
+		right = n;
+	}
+	//function to get right
+	public binary_node get_right(){
+		return right;
+	}
+	//to get left
+	public binary_node get_left(){
+		return left;
+	}
+	public void set_data(int d){
+		data = d;
+	}
+	public int getData(){
+		return   data;
+	}
 }
